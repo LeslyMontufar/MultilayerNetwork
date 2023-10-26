ef353043aaf2d53d598c860b52e1466c01edcbd9
Lesly <leslymontufarj@gmail.com>
2023-10-26 02:29:48 -0300

    Layer e Sample com sobrecarga no <<

--- definitions.h
+++ definitions.h
@@ -3,22 +3,45 @@
 
 #include <iostream>
 #include <vector>
-#include "samples.h"
 
 typedef double Number;
 
-std::ostream& operator<<(std::ostream& os, const std::vector<Sample>& samples){
-    for(const Sample& sample : samples){
-        os << "Entrada: ";
-        for(const Number& xi : sample.x) {
-            os << xi << " ";
+class Sample {       
+  public:
+  // attributes
+    std::vector<Number> x;
+    std::vector<Number> t;
+    int label;
+
+    Sample(const std::vector<Number>& x, const std::vector<Number>& target, const int& label)
+            : x(x), t(target), label(label) {}
+  
+  // friend permite que a função os membros privados da classe Sample
+  // mas quero que a MLP tbm tenha acesso entao declarei publico
+//   friend std::ostream& operator<<(std::ostream& os, const Sample& sample);
+    friend std::ostream& operator<<(std::ostream& os, const std::vector<Sample>& samples){
+        for(const Sample& sample : samples){
+            os << "Entrada: ";
+            for(const Number& xi : sample.x) {
+                os << xi << " ";
+            }
+            os << "\nTarget: ";
+            for(const Number& ti : sample.t) {
+                os << ti << " ";
+            }
+            os << "\nLabel: ";
+            os << sample.label;
+            os << "\n\n";
         }
-        os << "\nTarget: ";
-        for(const Number& ti : sample.t) {
-            os << ti << " ";
-        }
-        os << "\n\n";
+        return os;
     }
-}
+};
+
+std::vector<Sample> samples = {
+    Sample({-1, -1}, {-1}, 0),
+    Sample({1, -1}, {1}, 1),
+    Sample({-1, 1}, {1}, 1),
+    Sample({1, 1}, {1}, 1)
+};
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,23 +1,70 @@
 #include "definitions.h"
-#include "samples.h"
+#include <random>
+
+class Layer {
+  public:
+    int activation = 1; // 1 - Bipolar sigmoid
+    std::vector<Number> w;
+
+    Layer(const int& nNeurons, const int& activationF) : activation(activationF) {
+        // Inicialize o gerador de números aleatórios
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<Number> dis(-0.5, 0.5);
+
+        // Redimensione o vetor w e preencha com números aleatórios
+        w.resize(nNeurons + 1);  // +1 para o viés (bias)
+        for (int i = 0; i < (int) w.size(); i++) {
+            w[i] = dis(gen);
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Layer& layer) {
+        os << "Activation: " << layer.activation << "\nWeights: ";
+        for (const Number& weight : layer.w) {
+            os << weight << " ";
+        }
+        return os;
+    }
+};
 
 class MLP {
   private:
-    std::vector<Sample> trainingSample;
+    std::vector<Sample> trainingSamples;
+    int epochs = 10000;
+    Number alpha = 0.01;
+    std::vector<Layer> layers;
 
   public: 
-    MLP(const std::vector<Sample>& samples) : trainingSample(samples) {}
+    MLP(const std::vector<Sample>& samples) : 
+        trainingSamples(samples) {}
 
     // methods
-    void showInfo(){
-      std::cout << trainingSample;
+    void showTrainingSamples(){
+      std::cout << trainingSamples;
+    }
+
+    void train(){
+
+    }
+    void showTrainedNetwork(){
+      for(const Layer& layer : layers) {
+        std::cout << layer << "\n";
+      }
+        
+    }
+
+    void addLayer(const Layer& layer){
+      layers.push_back(layer);
     }
 
 };
 
 int main() {
   MLP network(samples);
-  network.showInfo();
-
+  // network.showTrainingSamples();
+  network.addLayer(Layer(3,1));
+  // network.train();
+  network.showTrainedNetwork();
   return 0;
 }
